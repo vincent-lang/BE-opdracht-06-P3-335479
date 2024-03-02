f43363e0a75d5a52ea02ba329ad4b62513c9f338
vincent van de merwe <335479@student.mboutrecht.nl>
2024-03-02 21:54:04 +0100

    next commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>homepage</title>
+</head>
+
+<body>
+    <div class="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+
+    <div class="title">
+        <h1>Snoepwinkel jamin</h1>
+    </div>
+    <div class="table">
+        <table>
+            <thead>
+                <tr>
+                    <th>Leveranciers</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><a href="{{route('leverancier.index')}}"><img src="/img/kruisje.png" alt="kruisje.png" class="small-img"></a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
